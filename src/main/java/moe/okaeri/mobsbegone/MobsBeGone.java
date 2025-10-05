0e391832ac33364b35f42a9347c998f86cd05691
kuroneko007 <sven.sorrell@gmail.com>
2025-10-05 11:12:14 +0200

    Added a check on the dimension an entity is in. Currently, applies the blacklist only to the Overworld (hard-coded); support for setting via JSON file planned for next version.

--- src/main/java/moe/okaeri/mobsbegone/MobsBeGone.java
+++ src/main/java/moe/okaeri/mobsbegone/MobsBeGone.java
@@ -14,6 +14,7 @@ import net.minecraft.registry.Registries;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.world.ServerWorld;
 
+import net.minecraft.world.World;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -41,7 +42,10 @@ public class MobsBeGone implements ModInitializer {
 	private void onEntityLoad(Entity entity, ServerWorld world) {
 		if (entity instanceof LivingEntity) {
 			if (isEntityBlacklisted(entity.getType())) {
-				ENTITY_BUFFER.add(entity);
+                //TODO: Remove hard-coding and add unwanted dimensions to Blacklist file
+                if (entity.getWorld().getRegistryKey() == World.OVERWORLD) {
+                    ENTITY_BUFFER.add(entity);
+                }
 			}
 		}
 	}
@@ -49,7 +53,10 @@ public class MobsBeGone implements ModInitializer {
 	private void onEndServerTick(MinecraftServer minecraftServer) {
 		for (int i = ENTITY_BUFFER.size(); i-- > 0;) {
 			Entity e = ENTITY_BUFFER.get(i);
-			if (e != null && !e.isRemoved()) e.discard();
+			if (e != null && !e.isRemoved()) {
+                e.discard();
+                // LOGGER.info("Event watcher discarded" + e.getType());
+            }
 		}
 		ENTITY_BUFFER.clear();
 	}
