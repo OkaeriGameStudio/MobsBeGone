cf5a4f4e2f69e3d4ac19dca6e2244f7c0e1b9194
EasyMochi <mochi@okaeri.moe>
2026-08-03 16:21:05 +0100

    Solved compatibility issue with Cobblemon beehive mixin.

--- src/main/java/moe/okaeri/mobsbegone/mixin/ServerWorldMixin.java
+++ src/main/java/moe/okaeri/mobsbegone/mixin/ServerWorldMixin.java
@@ -13,7 +13,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(ServerWorld.class)
 public class ServerWorldMixin {
-	// Returns true so callers (like MobSpawnerLogic and BeehiveBlockEntity) think the spawn succeeded, resetting their timers and preventing infinite tick spawn loops. (zonary123)
+	// Returns true so callers (like MobSpawnerLogic and BeehiveBlockEntity) think the spawn succeeded, resetting their timers and preventing infinite tick spawn loops. (Thank you zonary123!!)
 	@Inject(method = "addEntity", at = @At("HEAD"), cancellable = true)
 	private void onAddEntity(Entity entity, CallbackInfoReturnable<Boolean> cir) {
 		if (entity instanceof LivingEntity && MobsBeGone.isEntityBlacklisted(entity.getType())) {
