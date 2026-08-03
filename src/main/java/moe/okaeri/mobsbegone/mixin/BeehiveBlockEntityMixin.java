f937dc226a027fcec8821b0c68860bd5d53a76bd
EasyMochi <mochi@okaeri.moe>
2026-08-03 12:02:53 +0100

    Bee mixin constructor removed

--- src/main/java/moe/okaeri/mobsbegone/mixin/BeehiveBlockEntityMixin.java
+++ src/main/java/moe/okaeri/mobsbegone/mixin/BeehiveBlockEntityMixin.java
@@ -12,8 +12,6 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(BeehiveBlockEntity.class)
 public class BeehiveBlockEntityMixin {
-	private BeehiveBlockEntityMixin() { }
-
 	// Thank you zonary123 for this fix! Returns true to mark bees as already spawned to prevent the legendary 20Hz bee dubstep.
 	@Inject(method = "releaseBee", at = @At("HEAD"), cancellable = true)
 	private static void onReleaseBee(CallbackInfoReturnable<Boolean> cir) {
