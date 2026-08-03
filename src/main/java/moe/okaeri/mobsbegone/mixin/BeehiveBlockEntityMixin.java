7f2ddc311a5711569c7aa6e95c549956922b5792
EasyMochi <mochi@okaeri.moe>
2026-08-03 11:53:50 +0100

    Bee mixing code simplification and credits

--- src/main/java/moe/okaeri/mobsbegone/mixin/BeehiveBlockEntityMixin.java
+++ src/main/java/moe/okaeri/mobsbegone/mixin/BeehiveBlockEntityMixin.java
@@ -2,27 +2,21 @@ package moe.okaeri.mobsbegone.mixin;
 
 import moe.okaeri.mobsbegone.MobsBeGone;
 
-import net.minecraft.block.BlockState;
 import net.minecraft.block.entity.BeehiveBlockEntity;
-import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.World;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-import java.util.List;
-
 @Mixin(BeehiveBlockEntity.class)
 public class BeehiveBlockEntityMixin {
-
 	private BeehiveBlockEntityMixin() { }
 
+	// Thank you zonary123 for this fix! Returns true to mark bees as already spawned to prevent the legendary 20Hz bee dubstep.
 	@Inject(method = "releaseBee", at = @At("HEAD"), cancellable = true)
-	private static void onReleaseBee(World world, BlockPos pos, BlockState state, BeehiveBlockEntity.BeeData bee, List<Entity> entities, BeehiveBlockEntity.BeeState beeState, BlockPos flowerPos, CallbackInfoReturnable<Boolean> cir) {
+	private static void onReleaseBee(CallbackInfoReturnable<Boolean> cir) {
 		if (MobsBeGone.isEntityBlacklisted(EntityType.BEE)) {
 			cir.setReturnValue(true);
 			cir.cancel();
